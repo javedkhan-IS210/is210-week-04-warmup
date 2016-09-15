1c33628f436b3d84d5dea53cd3427e5d0f0766ae
javedkhan-IS210 <javed.khan@spsmail.cuny.edu>
2016-09-15 16:24:14 +0000

    added the function docstring

--- task_01.py
+++ task_01.py
@@ -4,7 +4,23 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """A function that knows what you mean
+    
+    Args:
+         wink (mixed):Arg to be arthmatically incremented to numwink.
+         numwink (mixed):Arg to increment wink. Default: 2.
+         winks optional(str): Arg to be multiplied by numwink and stripped of
+         no characters. 
+         nudges optional(str):Arg to muliply 'nudge' by numwink and stripped of
+         no characters.
+         retstr optional(str): Arg to concatenate optional parameters with a
+         string and comma,then inserts winks and nudges. 
+    Returns:
+         str: concatenates all three optional parameters inserting winks and
+         nudges into a string. 
+    """
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
     return retstr
+
